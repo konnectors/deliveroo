9213b8f83d03edb55c34a90ded6e24d649b8293e
Enguerran <enguerran@ticabri.com>
2018-06-07 19:18:14 +0200

    feat: Download invoices as pdf

--- src/index.js
+++ src/index.js
@@ -1,122 +1,159 @@
-const {
-  BaseKonnector,
-  requestFactory,
-  signin,
-  scrape,
-  saveBills,
-  log
-} = require('cozy-konnector-libs')
-const request = requestFactory({
-  // the debug mode shows all the details about http request and responses. Very usefull for
-  // debugging but very verbose. That is why it is commented out by default
-  // debug: true,
-  // activates [cheerio](https://cheerio.js.org/) parsing on each page
-  cheerio: true,
-  // If cheerio is activated do not forget to deactivate json parsing (which is activated by
-  // default in cozy-konnector-libs
-  json: false,
-  // this allows request-promise to keep cookies between requests
-  jar: true
-})
+const { BaseKonnector, scrape, saveBills, log } = require('cozy-konnector-libs')
+const cheerio = require('cheerio')
+const URL = require('url').URL
+const moment = require('moment')
+moment.locale('fr')
 
-const baseUrl = 'http://books.toscrape.com'
+const baseURL = new URL('https://deliveroo.fr/fr/login')
 
 module.exports = new BaseKonnector(start)
 
-// The start function is run by the BaseKonnector instance only when it got all the account
-// information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
-// the account information come from ./konnector-dev-config.json file
+const requestPromise = require('request-promise')
+const cookiejar = requestPromise.jar()
+
+const request = (() => {
+  let rp
+  function init() {
+    requestPromise.defaults({ jar: cookiejar })
+    return requestPromise
+  }
+  return (() => {
+    if (!rp) {
+      rp = init()
+    }
+    return rp
+  })()
+})()
+
 async function start(fields) {
   log('info', 'Authenticating ...')
   await authenticate(fields.login, fields.password)
   log('info', 'Successfully logged in')
-  // The BaseKonnector instance expects a Promise as return of the function
   log('info', 'Fetching the list of documents')
-  const $ = await request(`${baseUrl}/index.html`)
-  // cheerio (https://cheerio.js.org/) uses the same api as jQuery (http://jquery.com/)
+  const $ = await request({
+    uri: `${baseURL.origin}/fr/orders`,
+    transform: body => cheerio.load(body),
+    jar: cookiejar
+  })
   log('info', 'Parsing list of documents')
   const documents = await parseDocuments($)
 
-  // here we use the saveBills function even if what we fetch are not bills, but this is the most
-  // common case in connectors
   log('info', 'Saving data to Cozy')
-  await saveBills(documents, fields.folderPath, {
-    // this is a bank identifier which will be used to link bills to bank operations. These
-    // identifiers should be at least a word found in the title of a bank operation related to this
-    // bill. It is not case sensitive.
-    identifiers: ['books']
-  })
-}
-
-// this shows authentication using the [signin function](https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#module_signin)
-// even if this in another domain here, but it works as an example
-function authenticate(username, password) {
-  return signin({
-    url: `http://quotes.toscrape.com/login`,
-    formSelector: 'form',
-    formData: { username, password },
-    // the validate function will check if
-    validate: (statusCode, $) => {
-      // The login in toscrape.com always works excepted when no password is set
-      if ($(`a[href='/logout']`).length === 1) {
-        return true
-      } else {
-        // cozy-konnector-libs has its own logging function which format these logs with colors in
-        // standalone and dev mode and as JSON in production mode
-        log('error', $('.error').text())
-        return false
+  await saveBills(
+    documents.map(entry => ({
+      ...entry,
+      requestOptions: {
+        jar: cookiejar
       }
+    })),
+    fields.folderPath,
+    {
+      identifiers: ['deliveroo']
     }
-  })
+  )
 }
 
-// The goal of this function is to parse a html page wrapped by a cheerio instance
-// and return an array of js objects which will be saved to the cozy by saveBills (https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#savebills)
+async function authenticate(username, password) {
+  const options = {
+    formselector: 'form#login',
+    input: {
+      input_login_name: 'login_email',
+      input_password_name: 'login_password'
+    },
+    username,
+    password,
+    generateHeader: $ => ({
+      'x-csrf-token': $('meta[name="csrf-token"]').attr('content'),
+      'content-type': 'application/json;charset=UTF-8'
+    }),
+    validate: response => response && JSON.parse(response.body).user.id,
+    debug: false
+  }
+
+  return await signin(options)
+}
+
+async function signin({
+  formselector = 'form',
+  input = { input_login_name: 'email', input_password_name: 'password' },
+  username,
+  password,
+  generateHeader = () => ({}),
+  validate = () => false
+}) {
+  const loginurl = baseURL.href
+  const loginpage = await request({
+    uri: loginurl,
+    resolveWithFullResponse: true,
+    transform: (body, response, resolveWithFullResponse) =>
+      resolveWithFullResponse
+        ? { ...response, parsedBody: cheerio.load(body) }
+        : cheerio.load(body),
+    jar: cookiejar
+  }).catch(err => {
+    log('error', err)
+  })
+  const $ = loginpage.parsedBody
+  const formaction = $(formselector).attr('action')
+  const formurl = `${baseURL.origin}${formaction}`
+  const form = {
+    [$(`form input[name="${input.input_login_name}"]`).attr(
+      'inputmode'
+    )]: username,
+    [$(`form input[name="${input.input_password_name}"]`).attr(
+      'inputmode'
+    )]: password
+  }
+  const headers = generateHeader($)
+  const response = await request({
+    method: 'POST',
+    uri: formurl,
+    form,
+    headers,
+    resolveWithFullResponse: true,
+    jar: cookiejar
+  }).catch(err => {
+    log('error', err)
+  })
+  return validate(response)
+}
+
 function parseDocuments($) {
-  // you can find documentation about the scrape function here :
-  // https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#scrape
   const docs = scrape(
     $,
     {
       title: {
-        sel: 'h3 a',
-        attr: 'title'
+        sel:
+          'a:nth-child(1) > span:nth-child(1) > span:nth-child(2) > span:nth-child(1)'
       },
       amount: {
-        sel: '.price_color',
-        parse: normalizePrice
+        sel:
+          'a:nth-child(1) > span:nth-child(1) > span:nth-child(4) > span:nth-child(1)',
+        parse: text => parseFloat(/\d+,\d?/.exec(text)[0])
       },
-      fileurl: {
-        sel: 'img',
-        attr: 'src',
-        parse: src => `${baseUrl}/${src}`
+      date: {
+        sel: 'a:nth-child(1) > span:nth-child(1) > span:nth-child(3)',
+        parse: text => moment(text, 'D MMMM YYYY HH:mm').toDate()
       },
-      filename: {
-        sel: 'h3 a',
-        attr: 'title',
-        parse: title => `${title}.jpg`
+      numero: {
+        sel: 'a',
+        attr: 'href',
+        parse: src => src.split('/').pop()
       }
     },
-    'article'
+    'ul.results-list li'
   )
   return docs.map(doc => ({
     ...doc,
-    // the saveBills function needs a date field
-    // even if it is a little artificial here (these are not real bills)
-    date: new Date(),
-    currency: '€',
-    vendor: 'template',
+    currency: 'EUR',
+    vendor: 'deliveroo',
     metadata: {
-      // it can be interesting that we add the date of import. This is not mandatory but may be
-      // usefull for debugging or data migration
       importDate: new Date(),
-      // document version, usefull for migration after change of document structure
       version: 1
-    }
+    },
+    fileurl: `${baseURL.origin}/order/receipt/${doc.numero}`,
+    filename: `${doc.title.replace(' ', '-')}-${doc.numero}-${
+      doc.amount
+    }-EUR.pdf`
   }))
-}
-
-// convert a price string to a float
-function normalizePrice(price) {
-  return parseFloat(price.replace('£', '').trim())
 }
